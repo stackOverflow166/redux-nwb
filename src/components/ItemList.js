a96980ebfc9ff2f7b4b0e1582e3818f21e1aa525
wcypersonal <417177466@qq.com>
2018-11-24 22:58:14 +0800

    initial

---
+++ src/components/ItemList.js
@@ -0,0 +1,53 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { itemsFetchData } from '../actions/items';
+
+class ItemList extends Component {
+    componentDidMount() {
+        this.props.fetchData('http://5bf95f40c480680013bc7e32.mockapi.io/api/v1/items');
+    }
+
+    render() {
+        if (this.props.hasErrored) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+
+        if (this.props.isLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return (
+            <ul>
+                {this.props.items.banner.map((item) => (
+                    <li key={item.id}>
+                        {item.key}
+                    </li>
+                ))}
+                {console.log(this.props.items.banner)}
+            </ul>
+        );
+    }
+}
+
+ItemList.propTypes = {
+    fetchData: PropTypes.func.isRequired,
+    items: PropTypes.object.isRequired,
+    hasErrored: PropTypes.bool.isRequired,
+    isLoading: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.items,
+        hasErrored: state.itemsHasErrored,
+        isLoading: state.itemsIsLoading
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchData: (url) => dispatch(itemsFetchData(url))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
